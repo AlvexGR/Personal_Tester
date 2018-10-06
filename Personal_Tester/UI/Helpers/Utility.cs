72656896e20553a708e0c7fb6ce74d8e34cdb496
AlvexGR <goldenream1503@gmail.com>
2018-10-06 21:27:52 +0700

    Structure the solution

---
+++ Personal_Tester/UI/Helpers/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UI.Helpers
+{
+    public static class Utility
+    {
+        public static App GetApp()
+        {
+            return (App)Application.Current;
+        }
+    }
+}
---
+++ Personal_Tester/UI/ViewModels/Windows/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.ViewModels.Windows
+{
+    public interface IMainWindowViewModel : IBaseViewModel
+    {
+
+    }
+
+    public class MainWindowViewModel : ReactiveObject
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
