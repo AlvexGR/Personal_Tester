7f537c9c01b19d1108f463aa81fa54d240205ff5
AlvexGR <goldenream1503@gmail.com>
2018-10-07 17:27:39 +0700

    Added some models

---
+++ Personal_Tester/DomainLibrary/Models/ProblemModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLibrary.Models
+{
+    public interface IProblem
+    {
+    }
+
+    public class ProblemModel : IProblem
+    {
+    }
+}
